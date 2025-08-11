8bc89cd51c21b7868315bf1a9243589b685e4496
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-08-12 02:18:51 +0530

    Basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+
+const accountId = 12214532;
+let accountEmail = "udixit3647@gmail.com";
+var accountPassword = "12345";
+accountCity = "Delhi";
+
+// accountId=2;  not allowed 
+accountEmail = "Vansh@gmail.com";
+accountPassword = "67890";
+accountCity = "Mumbai";
+// console.log(accountEmail);
+// console.log(accountId);
+ /* prefer not to use var because of block scope and functional scope issue*/ 
+ let accountState;  // undefined
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
