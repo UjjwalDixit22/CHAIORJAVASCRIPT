a0ff16f1e04eac8910d112e71f35e3acdeadad24
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-08-12 10:32:12 +0530

    DataTypes

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -1,2 +1,20 @@
 
+ "use strict";// This line is optional but recommended to enforce strict mode in JavaScript
+ console.log("Data Types in JavaScript");
+ console.log(3+3);
+ console.log("Ujjwal");
+//  alert("Hello Ujjwal");// we are using node.js, so alert won't work
  
+let name = "Ujjwal";
+let age = 25;
+let isLoggedIn = true;
+// number => 2 pwer 53, string, boolean, null, undefined, BigInt, Symbol=> unique identifier 
+// object => collection of key value pairs
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof null); // object (this is a known JavaScript quirk)
+console.log(typeof undefined); // undefined
+console.log(typeof BigInt(12345678901234567890)); // bigint
+console.log(typeof Symbol("unique")); // symbol
