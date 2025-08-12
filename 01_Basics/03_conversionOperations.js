de8fcfda972ba81dcac9c52ea4aeefd702c574f8
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-08-12 13:10:14 +0530

    Conversion of DataTypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+// let score = "33abcd";
+// let score = null;
+// let score = undefined;
+// console.log(typeof score); // number
+// let valueinNumber = Number(score);
+// console.log(valueinNumber); // 33
+// console.log(typeof valueinNumber); // number
+
+
+// 33=> 33
+// "33abcd" => NaN (Not a Number)
+// null => 0
+// undefined => NaN 
+// boolean => 0 or 1
+
+// let isloggedin = 1;
+// let checkloggedin = Boolean(isloggedin);
+// console.log(checkloggedin); // true
+// console.log(typeof checkloggedin); // boolean
+
+// 1=> true
+// 0=> false
+// "Ujjwal" => true
+// "" => false
+
+
+let someNumber = 45;
+let stringNumber = String(someNumber);
+console.log(stringNumber); // "45"
+console.log(typeof stringNumber); // string
