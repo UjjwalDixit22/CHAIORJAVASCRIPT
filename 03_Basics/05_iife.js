05c0de2db820afe76a539c6a57eeb7f6367801bb
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-08-24 19:52:30 +0530

    IIFE

---
+++ 03_Basics/05_iife.js
@@ -0,0 +1,12 @@
+
+// IIFE (Immediately Invoked Function Expression)
+(function chai() { //named iife
+    console.log("This is an IIFE!");
+})();
+
+((name)=>{ //unnamed iife with parameter
+    console.log(`This is IIFE 2! ${name}`);
+})('Ujjwal Dixit');
+
+ 
+
