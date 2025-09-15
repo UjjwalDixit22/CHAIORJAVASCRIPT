0742ca82025a3f4bcae39953d64548cbca4d35df
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-09-16 03:19:10 +0530

    Call and this in JavaScript

---
+++ 10_OOPS/call.js
@@ -0,0 +1,13 @@
+// 
+function setUsername(username){
+    this.username = username;
+}
+
+function createUser(username,email,password){
+    setUsername.call(this,username);
+    this.email=email;
+    this.password=password;
+}
+const chai = new createUser("Golu","qwe@gmail.com","1234")
+console.log(chai);//createUser { email: 'qwe@gmail.com', password: '1234' }
+ 
